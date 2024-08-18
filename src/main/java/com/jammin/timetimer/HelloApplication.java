8c000b99871458983ff65083d54a8d8f96c6fe5b
Ben Summer <jackwanamaker49@gmail.com>
2024-08-18 09:53:02 -0400

    Moved images to the resources folder, and added the image that will display around the Timer face showing the time and ticks

--- src/main/java/com/jammin/timetimer/HelloApplication.java
+++ src/main/java/com/jammin/timetimer/HelloApplication.java
@@ -1,6 +1,9 @@
 package com.jammin.timetimer;
 
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
@@ -11,6 +14,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
 
@@ -18,30 +23,54 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage PrimaryStage) {
-        ImageView myImage = new ImageView(new Image("file:img/kurupt.png"));
+        //Adds all the necessary elements into the window
 
-        Arc myArc = createArc();
+        StackPane myStackPane = new StackPane();
+
+        VBox myVBox = new VBox(myStackPane);
+
+        Scene myScene = new Scene(myVBox, 400, 400);
+
+        WebView myWebView = createBackgroundImage(myStackPane);
+
+        Arc myArc = createArc(myStackPane);
+
+        myStackPane.getChildren().remove(myArc);
 
         TextField myTextField = createTextField(myArc);
 
-        StackPane myStackPane = new StackPane(myImage,myArc);
-
-        VBox myVBox = new VBox(myStackPane, myTextField);
-
-        Scene myScene = new Scene(myVBox);
+        myVBox.getChildren().add(myTextField);
 
         PrimaryStage.setScene(myScene);
         PrimaryStage.show();
 
     }
+    //Constructor for Background Image
+    public WebView createBackgroundImage(StackPane myStackPane){
+        WebView myWebView = new WebView();
+        Double svgWidth = myStackPane.getScene().getWidth();
+        Double svgHeight = myStackPane.getScene().getHeight();
+        String svgPath = getClass().getResource("/img/timer_ticks.svg").toExternalForm();
+        String svgContent = "<html><body><img src = '" + svgPath + "'></body></html>";
+        myWebView.getEngine().loadContent(svgContent);
+        //myWebView.widthProperty().bind()
+        myStackPane.getChildren().add(myWebView);
+        return myWebView;
+    }
 
-    public Arc createArc(){
+    //Constructor for my Arc
+    public Arc createArc(StackPane myStackPane){
         Arc myArc = new Arc(100,100,100,100,0,360);
         myArc.setFill(Color.GREEN);
         myArc.setType(ArcType.ROUND);
+        myStackPane.getChildren().add(myArc);
+
+        myArc.radiusXProperty().bind(myArc.getScene().heightProperty().divide(2.5));
+        myArc.radiusYProperty().bind(myArc.getScene().heightProperty().divide(2.5));
+
         return myArc;
     }
-
+    //Textfield to enter the time
     public TextField createTextField(Arc myArc){
         TextField myTextField = new TextField("Enter Time 1-60");
 
@@ -53,12 +82,12 @@ public class HelloApplication extends Application {
 
         return myTextField;
     }
-
+    //Filter to only allow for use of time between 1-60minutes
     public TextFormatter.Change filter(TextFormatter.Change myChange){
         if (myChange.getControlNewText().isEmpty()){
             return myChange;
         }
-        Integer myVal = Integer.parseInt(myChange.getControlNewText());
+        int myVal = Integer.parseInt(myChange.getControlNewText());
 
         return myVal >=0 && myVal <= 60 ? myChange : null;
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.jammin.timetimer {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.web;
 
 
     opens com.jammin.timetimer to javafx.fxml;
